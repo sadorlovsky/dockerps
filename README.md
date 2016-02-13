f944d1531f5da0728d7763f51720b2335775b4c6
Zach Eagle <mazaxakep@gmail.com>
2016-02-13 06:31:14 +0300

    first version

---
+++ README.md
@@ -0,0 +1,24 @@
+# dockerps
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+Pretty-print docker ps utility
+
+## Install
+```
+sudo npm install -g dockerps
+```
+
+## Usage
+```
+$ dockerps --help
+
+  Usage
+    $ dockerps [OPTIONS]
+
+  Options
+    -a, --all     Show all containers (default shows just running)
+    -l, --latest  Show the latest created container, include non-running
+    -n, --number  Show n last created containers, include non-running
+    -h, --help    Show usage
+```
---
+++ bin/dockerps.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict'
+
+const meow = require('meow')
+const _ = require('lodash')
+const exec = require('child_process').exec
+const print = require('../lib')
+
+const cli = meow(`
+  Usage
+    $ dockerps [OPTIONS]
+
+  Options
+    -a, --all     Show all containers (default shows just running)
+    -l, --latest  Show the latest created container, include non-running
+    -n, --number  Show n last created containers, include non-running
+    -h, --help    Show usage
+`,
+  {
+    alias: {
+      a: 'all',
+      l: 'latest',
+      n: 'number',
+      h: 'help'
+    }
+  }
+)
+
+const command = 'docker ps'
+
+let options = ''
+
+if (cli.flags.a) {
+  options += '-a'
+}
+
+if (cli.flags.l) {
+  options += '-l'
+}
+
+if (_.isNumber(cli.flags.n)) {
+  options += `-n ${cli.flags.n}`
+}
+
+const formatOptions = [
+  '{{.ID}}', '{{.Names}}', '{{.Image}}', '{{.Command}}',
+  '{{.CreatedAt}}', '{{.RunningFor}}', '{{.Status}}', '{{.Ports}}'
+]
+
+exec(`${command} ${options} --format "${_.join(formatOptions, '|')}"`,
+  (error, stdout, stderr) => {
+    console.log(print(stdout))
+    if (error !== null) {
+      console.log(error)
+    }
+  }
+)
