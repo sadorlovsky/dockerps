f15a479848e5cf43822c579a061eb956fa653188
Zach Eagle <mazaxakep@gmail.com>
2016-02-28 14:23:57 +0300

    execute test

---
+++ test/_helper.js
@@ -0,0 +1,37 @@
+import faker from 'faker'
+import moment from 'moment'
+
+export function fakeStdout () {
+  const stdout = []
+
+  for (let i = 0; i < (Math.random() * 100); i++) {
+    const id = Math.random().toString(36).substring(6)
+    const name = faker.helpers.slugify(`${faker.hacker.adjective()} ${faker.hacker.noun()}`)
+    const image = faker.helpers.slugify(faker.hacker.noun())
+    const command = 'npm start'
+    const createdAt = moment(faker.date.past()).format('YYYY-MM-DD HH:mm:ss ZZ')
+    const runningFor = moment(faker.date.past()).fromNow(true)
+    const status = () => {
+      const _status = faker.helpers.randomize(['Up', 'Exited'])
+      const days = faker.random.number()
+      if (_status === 'Exited') {
+        return `${_status} (0) ${days} days ago`
+      } else {
+        return `${_status} ${days} days`
+      }
+    }
+    const ports = () => {
+      return faker.helpers.randomize([
+        '0.0.0.0:1194->1194/udp',
+        '0.0.0.0:8080->8080/tcp',
+        '1194/udp',
+        '8080/tcp',
+        ''
+      ])
+    }
+
+    stdout.push(`${id}\t${name}\t${image}\t\"${command}\"\t${createdAt}\t${runningFor}\t${status()}\t${ports()}`)
+  }
+
+  return stdout.join('\n')
+}
---
+++ test/executeTest.js
@@ -0,0 +1,22 @@
+import proxyquire from 'proxyquire'
+import test from 'ava'
+import { fakeStdout } from './_helper'
+
+const stub = {}
+const execute = proxyquire('../src/execute', { 'child_process': stub })
+
+test('format()', (t) => {
+  const expected = '{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}'
+  t.same(execute.format(), expected)
+})
+
+test('execute()', (t) => {
+  stub.execSync = fakeStdout
+  t.ok(execute.execute())
+})
+
+test.skip('parse()', (t) => {
+  stub.execSync = fakeStdout
+  const stdout = execute.execute()
+  t.same(execute.parse(stdout), '')
+})
