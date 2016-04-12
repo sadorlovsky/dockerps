058bb6106101b6ffae1682f37f11ef7e3770b91b
Zach Orlovsky <sadorlovsky@gmail.com>
2016-04-13 01:33:04 +0300

    refactor execute

--- src/execute.js
+++ src/execute.js
@@ -1,63 +1,38 @@
 import child_process from 'child_process'
-import _ from 'lodash'
 
-export function optionsToString (flags) {
-  const options = []
+const format = () => [
+  '{{.ID}}',
+  '{{.Names}}',
+  '{{.Image}}',
+  '{{.Command}}',
+  '{{.CreatedAt}}',
+  '{{.RunningFor}}',
+  '{{.Status}}',
+  '{{.Ports}}'
+].join('\t')
 
-  if (flags.all || flags.a) {
-    options.push('-a')
-  }
-
-  if (flags.latest || flags.l) {
-    options.push('-l')
-  }
-
-  if (_.isNumber(flags.number || flags.n)) {
-    options.push(`-n ${flags.number || flags.n}`)
-  }
-
-  if (_.isString(flags.filter || flags.f)) {
-    options.push(`-f ${flags.filter || flags.f}`)
-  }
-
-  return options.join(' ')
+const exec = (options = '', _format = format()) => {
+  return child_process.execSync(`docker ps ${options} --format "${_format}"`).toString()
 }
 
-export function format () {
-  return [
-    '{{.ID}}',
-    '{{.Names}}',
-    '{{.Image}}',
-    '{{.Command}}',
-    '{{.CreatedAt}}',
-    '{{.RunningFor}}',
-    '{{.Status}}',
-    '{{.Ports}}'
-  ].join('\t')
+const parse = (stdout) => {
+  return stdout.split('\n')
+    .filter((row) => row !== '')
+    .map((row) => {
+      const data = row.split('\t')
+      return {
+        id: data[0],
+        name: data[1],
+        image: data[2],
+        command: data[3],
+        createdAt: data[4],
+        runningFor: data[5],
+        status: data[6],
+        ports: data[7]
+      }
+    })
 }
 
-export function execute (options) {
-  return child_process.execSync(`docker ps ${options || ''} --format "${format()}"`).toString()
-}
-
-export function parse (stdout) {
-  const containers = []
-
-  for (let row of stdout.split('\n')) {
-    if (row === '') continue
-    const data = row.split('\t')
-    const container = {
-      id: data[0],
-      name: data[1],
-      image: data[2],
-      command: data[3],
-      createdAt: data[4],
-      runningFor: data[5],
-      status: data[6],
-      ports: data[7]
-    }
-    containers.push(container)
-  }
-
-  return containers
+export default function execute (options) {
+  return parse(exec(options))
 }
--- test/execute.js
+++ test/execute.js
@@ -5,31 +5,24 @@ import { randomStdout } from './_helper'
 const stub = {}
 const execute = proxyquire('../src/execute', { 'child_process': stub })
 
-test('format()', (t) => {
-  const expected = '{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}'
-  t.deepEqual(execute.format(), expected)
-})
-
-test('execute()', (t) => {
+test('execute() works', (t) => {
   stub.execSync = randomStdout
-  t.truthy(execute.execute())
+  t.truthy(execute())
 })
 
-test('execute("-a")', (t) => {
+test('execute(\'-a\' works)', (t) => {
   stub.execSync = randomStdout
-  t.truthy(execute.execute('-a'))
+  t.truthy(execute('-a'))
 })
 
-test('parse no containers', (t) => {
+test('no containers', (t) => {
   stub.execSync = (cmd) => ''
-  const stdout = execute.execute()
   const expected = []
-  t.deepEqual(JSON.stringify(execute.parse(stdout)), JSON.stringify(expected))
+  t.deepEqual(execute(), expected)
 })
 
-test('parse single container', (t) => {
+test('single container', (t) => {
   stub.execSync = (cmd) => 'abc123def456\tmongo\tmongo\t\"./start.sh\"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days'
-  const stdout = execute.execute()
   const expected = [{
     id: 'abc123def456',
     name: 'mongo',
@@ -40,15 +33,14 @@ test('parse single container', (t) => {
     status: 'Up 5 days',
     ports: undefined
   }]
-  t.deepEqual(JSON.stringify(execute.parse(stdout)), JSON.stringify(expected))
+  t.deepEqual(execute(), expected)
 })
 
-test('parse two containers', (t) => {
+test('two containers', (t) => {
   stub.execSync = (cmd) => {
     return 'abc123def456\tmongo\tmongo\t\"./start.sh\"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days\n' +
       'abc123def457\tmongo\tmongo\t\"./start.sh\"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days'
   }
-  const stdout = execute.execute()
   const expected = [{
     id: 'abc123def456',
     name: 'mongo',
@@ -68,15 +60,14 @@ test('parse two containers', (t) => {
     status: 'Up 5 days',
     ports: undefined
   }]
-  t.deepEqual(JSON.stringify(execute.parse(stdout)), JSON.stringify(expected))
+  t.deepEqual(execute(), expected)
 })
 
-test('parse two containers with newline char', (t) => {
+test('two containers with the newline char', (t) => {
   stub.execSync = (cmd) => {
     return 'abc123def456\tmongo\tmongo\t\"./start.sh\"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days\n' +
       'abc123def457\tmongo\tmongo\t\"./start.sh\"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days\n'
   }
-  const stdout = execute.execute()
   const expected = [{
     id: 'abc123def456',
     name: 'mongo',
@@ -96,5 +87,5 @@ test('parse two containers with newline char', (t) => {
     status: 'Up 5 days',
     ports: undefined
   }]
-  t.deepEqual(JSON.stringify(execute.parse(stdout)), JSON.stringify(expected))
+  t.deepEqual(execute(), expected)
 })
