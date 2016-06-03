4107b9e2ae1e8e6b8cfa0f634e95a86a7f583cdc
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-03 11:21:30 +0300

    WIP

--- src/dockerps.js
+++ src/dockerps.js
@@ -1,42 +1,9 @@
-import child_process from 'child_process'
+import 'babel-polyfill'
+import got from 'got'
 
-const format = [
-  '{{.ID}}',
-  '{{.Names}}',
-  '{{.Image}}',
-  '{{.Command}}',
-  '{{.CreatedAt}}',
-  '{{.RunningFor}}',
-  '{{.Status}}',
-  '{{.Ports}}'
-].join('\t')
-
-function exec (options) {
-  return child_process
-    .execSync(`docker ps ${options} --format "${format}"`)
-    .toString()
+async function dockerps (dockerAddress = 'unix:///var/run/docker.sock') {
+  const data = await got(`${dockerAddress}/containers/json`)
+  return data.body
 }
 
-function ast (stdout) {
-  return stdout.split('\n')
-    .filter(row => row !== '')
-    .map(row => {
-      const data = row.split('\t')
-      return {
-        id: data[0],
-        name: data[1],
-        image: data[2],
-        command: data[3],
-        createdAt: data[4],
-        runningFor: data[5],
-        status: data[6],
-        ports: data[7]
-      }
-    })
-}
-
-export default function dockerps (options = { all: false }) {
-  const stdout = exec(options)
-  const result = ast(stdout)
-  return result
-}
+export default dockerps
--- test/dockerps.js
+++ test/dockerps.js
@@ -1,79 +1,18 @@
+import path from 'path'
 import test from 'ava'
+import nock from 'nock'
+import fs from 'mz/fs'
 import dockerps from '../src/dockerps'
 
 const stub = {}
 dockerps.__Rewire__('child_process', stub)
 
-test('no containers', t => {
-  stub.execSync = () => ''
-  t.deepEqual(dockerps({}), [])
-})
+test('get containers', async t => {
+  const fixturePath = path.resolve('fixtures', 'containers.json')
+  nock('http://docker.local')
+    .get('/containers/json')
+    .replyWithFile(200, fixturePath)
 
-test('single container', t => {
-  stub.execSync = () => 'abc123def456\tmongo\tmongo\t"./start.sh"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days'
-  const expected = [{
-    id: 'abc123def456',
-    name: 'mongo',
-    image: 'mongo',
-    command: '"./start.sh"',
-    createdAt: '2016-02-26 17:28:39 +0300',
-    runningFor: '5 days',
-    status: 'Up 5 days',
-    ports: undefined
-  }]
-  t.deepEqual(dockerps({}), expected)
-})
-
-test('two containers', t => {
-  stub.execSync = () => {
-    return 'abc123def456\tmongo\tmongo\t"./start.sh"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days\n' +
-      'abc123def457\tmongo\tmongo\t"./start.sh"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days'
-  }
-  const expected = [{
-    id: 'abc123def456',
-    name: 'mongo',
-    image: 'mongo',
-    command: '"./start.sh"',
-    createdAt: '2016-02-26 17:28:39 +0300',
-    runningFor: '5 days',
-    status: 'Up 5 days',
-    ports: undefined
-  }, {
-    id: 'abc123def457',
-    name: 'mongo',
-    image: 'mongo',
-    command: '"./start.sh"',
-    createdAt: '2016-02-26 17:28:39 +0300',
-    runningFor: '5 days',
-    status: 'Up 5 days',
-    ports: undefined
-  }]
-  t.deepEqual(dockerps({}), expected)
-})
-
-test('two containers with the newline char', t => {
-  stub.execSync = () => {
-    return 'abc123def456\tmongo\tmongo\t"./start.sh"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days\n' +
-      'abc123def457\tmongo\tmongo\t"./start.sh"\t2016-02-26 17:28:39 +0300\t5 days\tUp 5 days\n'
-  }
-  const expected = [{
-    id: 'abc123def456',
-    name: 'mongo',
-    image: 'mongo',
-    command: '"./start.sh"',
-    createdAt: '2016-02-26 17:28:39 +0300',
-    runningFor: '5 days',
-    status: 'Up 5 days',
-    ports: undefined
-  }, {
-    id: 'abc123def457',
-    name: 'mongo',
-    image: 'mongo',
-    command: '"./start.sh"',
-    createdAt: '2016-02-26 17:28:39 +0300',
-    runningFor: '5 days',
-    status: 'Up 5 days',
-    ports: undefined
-  }]
-  t.deepEqual(dockerps({}), expected)
+  const result = await dockerps('http://docker.local')
+  t.deepEqual(result, await fs.readFile(fixturePath, { encoding: 'utf8' }))
 })
