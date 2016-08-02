c38a095a340ee9ebde0faa21584af8608ade55d0
Zach Orlovsky <sadorlovsky@gmail.com>
2016-08-03 00:51:09 +0300

    refactor test

--- test/dockerps.test.js
+++ test/dockerps.test.js
@@ -2,34 +2,58 @@ import path from 'path'
 import test from 'ava'
 import Mitm from 'mitm'
 import fs from 'mz/fs'
-import dockerps from '../src/dockerps'
+import dockerps from '../lib/dockerps'
 
-test('get running containers', async t => {
-  const fixturePath = path.resolve('fixtures', 'containers.json')
-  const containers = JSON.parse(await fs.readFile(fixturePath, { encoding: 'utf8' }))
-    .filter(c => c['State'] === 'running')
-  const expected = JSON.stringify(containers)
+test('running containers', async t => {
+  const fixtures = JSON.stringify([{
+    "Id": "123abc456def789ghi123jkl456mno789pqr123stu456vwx789yza123bcd456ef",
+    "Names": ["/hello-world"],
+    "Image": "hello-world",
+    "Command": "/hello",
+    "Created": 1000000,
+    "Ports": [],
+    "State": "running",
+    "Status": "Up 5 minutes"
+  }, {
+    "Id": "abc456def789ghi123jkl456mno789pqr123stu456vwx789yza123bcd456ef123",
+    "Names": ["/foobar"],
+    "Image": "foo-bar",
+    "Command": "ls -l",
+    "Created": 2000000,
+    "Ports": [{
+      "PrivatePort": 2015,
+      "Type": "tcp"
+    }],
+    "State": "running",
+    "Status": "Up 25 minutes"
+  }])
+
+  const expected = [{
+    id: '123abc456def',
+    image: 'hello-world',
+    name: 'hello-world',
+    command: '/hello',
+    created: 1000000,
+    state: 'running',
+    status: 'Up 5 minutes',
+    ports: ''
+  }, {
+    id: 'abc456def789',
+    image: 'foo-bar',
+    name: 'foobar',
+    command: 'ls -l',
+    created: 2000000,
+    state: 'running',
+    status: 'Up 25 minutes',
+    ports: '2015/tcp, ' // TODO: '2015/tcp'
+  }]
 
   const mitm = Mitm()
   mitm.on('request', (req, res) => {
     res.statusCode = 200
-    res.end(expected)
+    res.end(fixtures)
   })
 
   const result = await dockerps()
-  t.deepEqual(result, expected)
-})
-
-test('get all containers', async t => {
-  const fixturePath = path.resolve('fixtures', 'containers.json')
-  const expected = await fs.readFile(fixturePath, { encoding: 'utf8' })
-
-  const mitm = Mitm()
-  mitm.on('request', (req, res) => {
-    res.statusCode = 200
-    res.end(expected)
-  })
-
-  const result = await dockerps({ all: true })
   t.deepEqual(result, expected)
 })
